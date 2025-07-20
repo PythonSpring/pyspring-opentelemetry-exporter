f6ad4dd2137f20a8a1c17deff2828d67e884fb9c
William Chen <william_w_chen@trendmicro.com>
2025-07-21 02:05:41 +0800

    Update dependencies and enhance response tracing middleware
    - Updated py-spring-core dependency version to 0.0.23 in pyproject.toml.
    - Added ResponseTraceMiddleware to the exporter, improving request processing capabilities.
    - Updated __init__.py to include ResponseTraceMiddleware in the module's exports.

--- pyspring_opentelemetry_exporter/__init__.py
+++ pyspring_opentelemetry_exporter/__init__.py
@@ -1,10 +1,12 @@
+from pyspring_opentelemetry_exporter._response_trace_middleware import ResponseTraceMiddleware
 from ._exporter import provider_opentelemetry_exporter
 from ._request_hook_handler import RequestHookHandler, provide_default_request_hook_handler
 
 __all__ = [
+    "provide_default_request_hook_handler",
     "provider_opentelemetry_exporter", 
     "RequestHookHandler", 
-    "provide_default_request_hook_handler"
+    "ResponseTraceMiddleware",
 ]
 
 __version__ = "0.0.1"
