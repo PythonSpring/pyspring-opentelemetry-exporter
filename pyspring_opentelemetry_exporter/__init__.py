ec55c9909ef9ed6187a4aabfd9404d52015d815d
William Chen <william_w_chen@trendmicro.com>
2025-07-21 02:09:07 +0800

    Refactor imports in __init__.py and add OpenTelemetry trace to exports
    - Updated import statements to use absolute paths for better clarity.
    - Added 'trace' to the module's exports, enhancing the integration with OpenTelemetry.

--- pyspring_opentelemetry_exporter/__init__.py
+++ pyspring_opentelemetry_exporter/__init__.py
@@ -1,12 +1,14 @@
 from pyspring_opentelemetry_exporter._response_trace_middleware import ResponseTraceMiddleware
-from ._exporter import provider_opentelemetry_exporter
-from ._request_hook_handler import RequestHookHandler, provide_default_request_hook_handler
+from pyspring_opentelemetry_exporter._exporter import provider_opentelemetry_exporter
+from pyspring_opentelemetry_exporter._request_hook_handler import RequestHookHandler, provide_default_request_hook_handler
+from opentelemetry import trace
 
 __all__ = [
     "provide_default_request_hook_handler",
     "provider_opentelemetry_exporter", 
     "RequestHookHandler", 
     "ResponseTraceMiddleware",
+    "trace"
 ]
 
 __version__ = "0.0.1"
